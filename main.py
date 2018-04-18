2bd8b2a356c0432c8ce6baaee677320c8fce2db4
buaabai <baijinyu0053@sina.com>
2018-04-18 11:13:29 +0000

    complete main function

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from torch.autograd import Variable,Function
 import torch.optim as optim
 from torchvision import datasets, transforms
 import torch.nn.functional as F
-import model
+import model as M
 import util
 import argparse
 
@@ -28,6 +28,8 @@ def ParseArgs():
                         help='disable CUDA training')
     parser.add_argument('--seed',type=int,default=1,mevatar='S',
                         help='random seed(default: 1)')
+    parser.add_argument('--log-interval',type=int,default=100,mevatat='N',
+                        help='how many batches to wait before logging training status')
     
     args = parser.parse_args()
     args.cuda = not args.no_cuda and torch.cuda.is_available()
@@ -38,8 +40,12 @@ def main():
     if args.cuda:
         torch.cuda.manual_seed(args.seed)
     kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
+
     BATCH_SIZE = args.batch_size
     TEST_BATCH_SIZE = args.test_batch_size
+    learning_rate = args.lr
+    #momentum = args.momentum
+    weight_decay = args.weight_decay
 
     ###################################################################
     ##             Load Train Dataset                                ##
@@ -61,6 +67,22 @@ def main():
                        transforms.Normalize((0.1307,), (0.3081,))
                    ])),
     batch_size=TEST_BATCH_SIZE, shuffle=True,**kwargs)
+    model = M.LeNet5_Bin()
+    if args.cuda:
+        model.cuda()
+    criterion = nn.CrossEntropyLoss()
+    if args.cuda:
+        criterion.cuda()
+    #optimizer = optim.SGD(model.parameters(),lr=learning_rate,momentum=momentum)
+    optimizer = optim.Adam(model.parameters(),lr=learning_rate,weight_decay=weight_decay)
+    bin_op = util.Binop(model)
+
+    for epoch_index in range(1,args.epochs+1):
+        adjust_learning_rate(learning_rate,optimizer,epoch_index,args.lr_epochs)
+        train(args,epoch_index,train_loader,model,optimizer,criterion,bin_op)
+        test(args,test_loader,bin_op,criterion)
+    bin_op.Binarization()
+    save_model(model,best_acc)
     
 
 def save_model(model,acc):
@@ -72,23 +94,12 @@ def save_model(model,acc):
     torch.save(state,'model_state.pkl')
     print('*** DONE! ***')
 
-learning_rate = 1e-4
-weight_decay = 0.0001
 
-
-model = model.LeNet5_Bin()
-model.cuda()
-
-criterion = nn.CrossEntropyLoss()
-criterion.cuda()
-optimizer = optim.Adam(model.parameters(),lr=learning_rate,weight_decay=weight_decay)
-
-bin_op = util.Binop(model)
-
-def train(epoch):
+def train(args,epoch_index,train_loader,model,optimizer,criterion,bin_op):
     model.train()
     for batch_idx,(data,target) in enumerate(train_loader):
-        data,target = data.cuda(),target.cuda()
+        if args.cuda:
+            data,target = data.cuda(),target.cuda()
         data,target = Variable(data),Variable(target)
 
         optimizer.zero_grad()
@@ -104,12 +115,12 @@ def train(epoch):
 
         optimizer.step()
 
-        if batch_idx % 100 == 0:
+        if batch_idx % args.log_interval == 0:
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                epoch, batch_idx * len(data), len(train_loader.dataset),
+                epoch_index, batch_idx * len(data), len(train_loader.dataset),
                 100. * batch_idx / len(train_loader), loss.data[0]))
 
-def test():
+def test(model,test_loader,bin_op,criterion):
     global best_acc
     best_acc = 0.0
     model.eval()
@@ -136,17 +147,11 @@ def test():
         test_loss, correct, len(test_loader.dataset),
         100. * correct / len(test_loader.dataset)))
     
-def adjust_learning_rate(optimizer,epoch,lr_epoch):
-    lr = learning_rate * (0.1 ** (epoch // lr_epoch))
+def adjust_learning_rate(learning_rate,optimizer,epoch_index,lr_epoch):
+    lr = learning_rate * (0.1 ** (epoch_index // lr_epoch))
     for param_group in optimizer.param_groups:
         param_group['lr'] = lr
         return lr
 
 if __name__ == '__main__':
-    #torch.cuda.manual_seed(1)
-    for epoch in range(1000):
-        adjust_learning_rate(optimizer,epoch,50)
-        train(epoch)
-        test()
-    bin_op.Binarization()
-    save_model(model,best_acc)
+    main()
